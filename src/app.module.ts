405304ce5b97a2883e989fccc869f3124acda167
Shin Sungyun <sinsungyun1630@gmail.com>
2023-08-23 06:41:51 +0900

    Fix : 로그 미들웨어 적용 해제

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,8 +27,10 @@ import { RedisModule } from './redis/redis.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggingMiddleware).forRoutes('*');
-  }
-}
+
+// export class AppModule implements NestModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer.apply(LoggingMiddleware).forRoutes('*');
+//   }
+// }
+export class AppModule {}
